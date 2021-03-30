0c22b879fe929acf8123406f528d6ee6c69e15c6
ruslaw13 <rs3.sebas@gmail.com>
2021-03-30 16:33:54 +0200

    mijn first commit

--- debug-it-2.js
+++ debug-it-2.js
@@ -6,8 +6,8 @@ var BMI = weight / (heigth * heigth);
 console.log("Your BMI is: " + BMI.toFixed());
 
 if (BMI > 25) {
-  console.log(Depending on your build, you might be overweight");
-} else (BMI < 18) {
+  console.log("Depending on your build, you might be overweight");
+} if (BMI < 18) {
   console.log("Depending on your build, you might be underweight");
 } else {
   console.log("Looks like you're height / weight ratio is pretty healthy");
--- debug-it.js
+++ debug-it.js
@@ -5,10 +5,10 @@ var BMI = weight / (height * height);
 
 console.log("Your BMI is: " + BMI.tofixed());
 
-if (BMI > 25 {
+if (BMI > 25 ) {
   console.log("Depending on your build, you might be overweight");
 } else if (BMI < 18) {
-  console.log("Depending on your build, you might be underweight);
+  console.log("Depending on your build, you might be underweight");
 } else {
   console.log("Looks like you're height / weight ratio is pretty healthy");
 }
